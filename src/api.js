305a4aeb8b19d711ffc45411006d264b5957a89a
shubhp2610 <shubhpatel2610@outlook.com>
2020-11-03 09:00:50 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -5,7 +5,13 @@ const app = express();
 const router = express.Router();
 
 router.get('/',(req,res)=>{
- res.json({"SUCCESS" : true});
+ var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < 40 ; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+ res.json({"SUCCESS" : result});
 });
 
 app.use(`/.netlify/functions/api`, router);
